c550b91151e5e5f8e3d313495dfc96e6a8fbcabb
user <kamilulyalibek@gmail.com>
2024-01-03 20:54:52 +0600

    feat: authorization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {FC, useContext, useEffect, useState} from 'react';
 import logo from './logo.svg';
+import { observer } from 'mobx-react-lite';
 import './App.css';
+import LoginForm from './components/LoginForm';
+import { Context } from '.';
+import { count } from 'console';
+import UserService from './services/UserService';
 
-function App() {
+
+const App: FC = () => {
+
+  const {} = useContext(Context);
+  const [users,setUsers] = useState<IUser[]>([]);
+  useEffect(() => {
+    if (localStorage.getItem('token')){
+      store.checkAuth()
+    }
+  }, [])
+
+  async function getUsers() {
+    try{
+        const response = await UserService.fetchUsers();
+        setUsers(response.data);
+    }catch(e) {
+      console.log(e);
+    }
+  }
+
+  if (store.isLoading){
+    return <div>Loading...</div>
+  }
+
+  if (!store.isAuth) {
+    return (
+      <div>
+        <LoginForm/>
+        <button onClick={getUsers}>Get users</button>
+        </div>
+      
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div>
+        <h1>{store.isAuth ? `You are already registered ${store.user.email}` : 'SIGN UP' }</h1>
+        <h1>{store.user.isActivated ?  'Account has been verified by mail' : 'Verify youre account ' }</h1>
+        <button onClick={()=> store.logout()}></button>
+        <div>
+          <button onClick={getUsers}>Get users</button>
+        </div>
+        {users.map(user =>
+            <div key= {user.email}> {user.email}</div>   
+        )}
+      </div>
 
-export default App;
+  );
+;}
+export default observer(App);
